eb7baa5e8c413c14a73c66311a44c0ac5f607fd9
caped-baldys <119095339+caped-baldys@users.noreply.github.com>
2022-12-28 16:01:04 +0530

    updated modified:   test.py

--- test.py
+++ test.py
@@ -1,27 +1,39 @@
-"""Question 1. Write a for loop to iterate through the list A = [1, 2, 3, 4, 5, 6]. Square each element of the list in one by one fashion and print them. After the end of the iteration, print - "The sequence has ended"."""
+# """Question 1. Write a for loop to iterate through the list A = [1, 2, 3, 4, 5, 6]. Square each element of the list in one by one fashion and print them. After the end of the iteration, print - "The sequence has ended"."""
 
 
-A = [x for x in range(1,7)]
+# A = [x for x in range(1,7)]
 
-A_result = list(map(lambda x ,:x**2,A))
+# A_result = list(map(lambda x ,:x**2,A))
 
-for i in range(len(A_result)):print(A_result[i])
-print("The sequence has ended")
+# for i in range(len(A_result)):print(A_result[i])
+# print("The sequence has ended")
 
 
-"""Question 2. If choice of user = 2, print the pattern - > """
+# """Question 2. If choice of user = 2, print the pattern - > """
 
-a = 5
-choice = int(input('Choice->'))
-if(choice==1 or choice==2):
+# a = 5
+# choice = int(input('Choice->'))
+# if(choice==1 or choice==2):
 
-    for i in range(a,0,-1):
-        print('*'*i)
-    if choice == 2:
-        for i in range(1,a+1):
-            print('_'*i)
-else:
-    print('Invalid input')
+#     for i in range(a,0,-1):
+#         print('*'*i)
+#     if choice == 2:
+#         for i in range(1,a+1):
+#             print('_'*i)
+# else:
+#     print('Invalid input')
+
+
+
+'''Question 3. '''
+
+t_1 = (1, 4, 9, 16, 25, 36)
+t_modified = tuple((x**2 for x in t_1 ))
+print(t_modified[4])
+t_sliced = t_modified[1:3]
+print(t_sliced)
+#or
+#t_modified = *(x**2 for x in t_1 ),
 
 
 
