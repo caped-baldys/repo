8905508304d95ef6b37a1b6b5de78f33b41bf72f
caped-baldys <119095339+caped-baldys@users.noreply.github.com>
2022-12-29 19:05:16 +0530

    complete

--- que_7.py
+++ que_7.py
@@ -1,9 +1,16 @@
 '''Question 7'''
 
 list_a = [1, 2, 3, 4, 5]
-index = int(input('Index:'))
+
 try:
+    # input non integer value to raise valueerror 
+    # input value grater than size of list_a i:e 4
+    index = int(input('Index:')) 
+
     list_a[index]
 
-except:
-    print('error')
+except IndexError:
+    print('error Index')
+
+except ValueError:
+    print('error Value')
